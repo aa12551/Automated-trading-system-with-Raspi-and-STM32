0946d3c1c1a876ca5412e994570f565d5fc46f98
aa12551 <94910883+aa12551@users.noreply.github.com>
2022-09-14 00:41:21 +0800

    Add files via upload

---
+++ Final_project/Debug/Core/startup/startup_stm32f407xx.d
@@ -0,0 +1,1 @@
+Core/startup/startup_stm32f407xx.o: ../Core/startup/startup_stm32f407xx.s
---
+++ Final_project/Debug/Core/startup/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+S_SRCS += \
+../Core/startup/startup_stm32f407xx.s 
+
+OBJS += \
+./Core/startup/startup_stm32f407xx.o 
+
+S_DEPS += \
+./Core/startup/startup_stm32f407xx.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/startup/%.o: ../Core/startup/%.s Core/startup/subdir.mk
+	arm-none-eabi-gcc -mcpu=cortex-m4 -g3 -DDEBUG -c -x assembler-with-cpp -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@" "$<"
+
+clean: clean-Core-2f-startup
+
+clean-Core-2f-startup:
+	-$(RM) ./Core/startup/startup_stm32f407xx.d ./Core/startup/startup_stm32f407xx.o
+
+.PHONY: clean-Core-2f-startup
+
