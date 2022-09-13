bbf0bf9ff4116da535b2df44cc7ec2ce7bfd9abf
aa12551 <94910883+aa12551@users.noreply.github.com>
2022-09-14 00:47:18 +0800

    Add files via upload

---
+++ Final_project/Debug/FreeRTOS/list.d
@@ -0,0 +1,25 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/FreeRTOS.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/FreeRTOSConfig.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/projdefs.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/portable.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/deprecated_definitions.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/portable/ARM_CM4F/portmacro.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/mpu_wrappers.h \
+ C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/list.h
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/FreeRTOS.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/FreeRTOSConfig.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/projdefs.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/portable.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/deprecated_definitions.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/portable/ARM_CM4F/portmacro.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/mpu_wrappers.h:
+
+C:/Users/s9400/STM32CubeIDE/workspace_1.8.0/Final_project/FreeRTOS/include/list.h:
